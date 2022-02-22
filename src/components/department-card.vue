58f1da627bf17061acaa591f025f7d8a1cf8f181
Gideon Assafuah <gassafuah@gmail.com>
2022-02-22 11:27:27 +0000

    [IMP] department images

--- src/components/department-card.vue
+++ src/components/department-card.vue
@@ -21,19 +21,14 @@ export default {
   <div class="card" style="cursor:pointer;" @click="selectDepartment">
     <div class="card-body p-0">
       <div class="media py-4 px-3">
-        <div class="media-body">
+        <div class="media-body d-flex align-items-center">
           <span
             class="text-muted text-uppercase font-size-12 font-weight-bold"
             >{{ department.name }}</span
           >
         </div>
         <div class="align-self-center">
-          <span>
-            <feather
-              :type="`${department.icon}`"
-              :class="`icon-lg align-middle icon-dual-${department.color}`"
-            ></feather>
-          </span>
+            <img :src="department.icon" alt="" style="height:60px; border-radius:100px;" />
         </div>
       </div>
     </div>
