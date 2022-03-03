e302f862992740eb2370c0f0b95b4bf53d1d9841
Gideon Assafuah <gassafuah@gmail.com>
2022-03-03 15:46:25 +0000

    [IMP] task creation

--- src/router/views/pages/consulting/proposals/details.vue
+++ src/router/views/pages/consulting/proposals/details.vue
@@ -10,11 +10,28 @@
           <div class="card">
             <div class="card-body">
               <div class="row align-items-center">
-                <div class="col-xl-10 col-lg-9">
+                <div class="col-xl-12 col-lg-12">
                   <div class="mt-4 mt-lg-0">
                     <h5 class="mt-0 mb-1 font-weight-bold">{{
                       proposal.title
                     }}</h5>
+
+                    <div class="d-flex items-align-center mt-4">
+                      <div>
+                        <div
+                          class="badge badge-soft-primary font-size-13 font-weight-normal"
+                          >{{
+                            proposal.project_type && proposal.project_type.name
+                          }}</div
+                        >
+                      </div>
+                      <div>
+                        <div
+                          class="badge badge-soft-success font-size-13 font-weight-normal ml-5"
+                          >{{ proposal.funding_option }}</div
+                        >
+                      </div>
+                    </div>
                     <div class="row">
                       <div class="col-md-3">
                         <div class="mt-4">
@@ -32,19 +49,34 @@
                             Submission Date
                           </p>
                           <h5 class="font-size-16">{{
-                            proposal.submission_date
+                            proposal.submission_date || 'N/A'
                           }}</h5>
                         </div>
                       </div>
 
                       <div class="col-md-3">
-                        <button
-                          id="btn-new-event"
-                          class="btn btn-primary mt-4 mr-3"
-                          @click="showmodal = true"
+                        <b-dropdown
+                          class="d-inline"
+                          variant="link"
+                          toggle-class="font-weight-bold p-0 align-middle"
                         >
-                          <i class="uil-plus-circle"></i> Create New Report
-                        </button>
+                          <template v-slot:button-content>
+                            <button
+                              id="btn-new-event"
+                              class="btn btn-primary mt-4 mr-3"
+                            >
+                              <i class="uil-plus-circle"></i> Create New Report
+                            </button>
+                          </template>
+                          <b-dropdown-item
+                            v-for="report in reportTypes"
+                            :key="report.id"
+                            href="javascript: void(0);"
+                            variant="seconday"
+                          >
+                            {{ report.name }}
+                          </b-dropdown-item>
+                        </b-dropdown>
                       </div>
 
                       <div class="col-md-3">
@@ -53,18 +85,6 @@
                         </button>
                       </div>
                     </div>
-
-                    <div
-                      class="badge badge-soft-primary font-size-13 font-weight-normal mt-2"
-                      >{{
-                        proposal.project_type && proposal.project_type.name
-                      }}</div
-                    >
-
-                    <div
-                      class="badge badge-soft-success font-size-13 font-weight-normal ml-5"
-                      >{{ proposal.funding_option }}</div
-                    >
                   </div>
                 </div>
               </div>
@@ -74,8 +94,46 @@
           <!-- end card -->
         </div>
 
-        <div
-          v-if="!loading && !proposal.expression_of_interest && !proposal.financial_document_path && !proposal.technical_proposal"
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              <h5>Proposal Reports</h5>
+              <div class="row mt-5">
+                <div v-for="n in reportTypes" :key="n.id" class="col-md-4">
+                  <div class="p-2 border rounded mb-3">
+                    <div class="media">
+                      <div class="avatar-sm font-weight-bold mr-3">
+                        <span
+                          class="avatar-title rounded bg-soft-primary text-primary"
+                        >
+                          <i class="uil-file-plus-alt font-size-18"></i>
+                        </span>
+                      </div>
+                      <div class="media-body">
+                        <a href="#" class="d-inline-block mt-2">
+                          {{n.name}}.docx
+                        </a>
+                      </div>
+                      <div class="float-right mt-1">
+                        <a href="#" class="p-2">
+                          <i class="uil-download-alt font-size-18"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- <div
+          v-if="
+            !loading &&
+              !proposal.expression_of_interest &&
+              !proposal.financial_document_path &&
+              !proposal.technical_proposal
+          "
           class=" w-100 d-flex justify-content-center"
         >
           <img
@@ -83,7 +141,7 @@
             alt="no projects"
             style="width:30%"
           />
-        </div>
+        </div> -->
         <!-- end col-12 -->
       </div>
     </Layout>
@@ -122,6 +180,24 @@ export default {
       ],
       proposal: {},
       loading: false,
+      reportTypes: [
+        {
+          id: 1,
+          name: 'Expression of interest Report',
+        },
+        {
+          id: 2,
+          name: 'Technical Report',
+        },
+        {
+          id: 3,
+          name: 'Financial Report',
+        },
+        {
+          id: 4,
+          name: 'Techical & Financial Report',
+        },
+      ],
     }
   },
   created() {
